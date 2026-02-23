b9da3cfc9a2e08c93239e1d5e371fae746160f63
hume <13506491+shsw228@users.noreply.github.com>
2026-02-23 20:07:37 +0900

    Initial Commit

---
+++ KeyMapRender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyMapRender
+//
+//  Created by Kengo Tate on 2026/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyMapRender/KeyMapRenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyMapRenderApp.swift
+//  KeyMapRender
+//
+//  Created by Kengo Tate on 2026/02/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyMapRenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
